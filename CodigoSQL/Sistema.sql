cb5b406b0b9117e4115013593ccad217fa5a32d6
Andres <andresangel5c@gmail.com>
2024-10-04 12:24:35 -0400

    actualizacion de base de datos

--- CodigoSQL/Sistema.sql
+++ CodigoSQL/Sistema.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 03-10-2024 a las 19:30:46
+-- Tiempo de generación: 04-10-2024 a las 16:22:37
 -- Versión del servidor: 11.5.2-MariaDB
--- Versión de PHP: 8.3.10
+-- Versión de PHP: 8.3.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -30,7 +30,36 @@ SET time_zone = "+00:00";
 CREATE TABLE `Aulas` (
   `id` int(11) NOT NULL,
   `año` varchar(3) NOT NULL,
-  `seccion` varchar(3) NOT NULL
+  `seccion` varchar(3) NOT NULL,
+  `docente_guia` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Añoescolar`
+--
+
+CREATE TABLE `Añoescolar` (
+  `id` int(11) NOT NULL,
+  `año` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Docentes`
+--
+
+CREATE TABLE `Docentes` (
+  `id` int(11) NOT NULL,
+  `cedula` varchar(25) NOT NULL,
+  `primer_nombre` varchar(25) NOT NULL,
+  `segundo_nombre` varchar(25) DEFAULT NULL,
+  `primer_apellido` varchar(25) NOT NULL,
+  `segundo_apellido` varchar(25) DEFAULT NULL,
+  `telefono` varchar(20) DEFAULT NULL,
+  `dirrecion` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -46,7 +75,8 @@ CREATE TABLE `Estudiantes` (
   `segundo_nombre` varchar(30) DEFAULT NULL,
   `primer_apellido` varchar(30) NOT NULL,
   `segundo_apellido` varchar(30) DEFAULT NULL,
-  `fecha_nacimiento` varchar(20) NOT NULL
+  `fecha_nacimiento` varchar(20) NOT NULL,
+  `cedula_rep` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -76,8 +106,24 @@ INSERT INTO `Materias` (`Codigo`, `nombre`) VALUES
 --
 
 CREATE TABLE `Matricula` (
+  `id` int(11) NOT NULL,
   `id_aula` int(11) NOT NULL,
   `cedula_estuadiante` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Representantes`
+--
+
+CREATE TABLE `Representantes` (
+  `id` int(11) NOT NULL,
+  `cedula` varchar(25) NOT NULL,
+  `nombres` varchar(50) NOT NULL,
+  `apellidos` varchar(50) NOT NULL,
+  `telefono` varchar(20) DEFAULT NULL,
+  `direccion` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -101,14 +147,29 @@ CREATE TABLE `Usuario` (
 -- Indices de la tabla `Aulas`
 --
 ALTER TABLE `Aulas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `docente_guia` (`docente_guia`);
+
+--
+-- Indices de la tabla `Añoescolar`
+--
+ALTER TABLE `Añoescolar`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `Docentes`
+--
+ALTER TABLE `Docentes`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `cedula` (`cedula`);
 
 --
 -- Indices de la tabla `Estudiantes`
 --
 ALTER TABLE `Estudiantes`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `cedula` (`cedula`);
+  ADD KEY `cedula` (`cedula`),
+  ADD KEY `cedula_rep` (`cedula_rep`);
 
 --
 -- Indices de la tabla `Materias`
@@ -120,8 +181,16 @@ ALTER TABLE `Materias`
 -- Indices de la tabla `Matricula`
 --
 ALTER TABLE `Matricula`
+  ADD PRIMARY KEY (`id`),
   ADD KEY `id_aula` (`id_aula`),
   ADD KEY `cedula_estuadiante` (`cedula_estuadiante`);
+
+--
+-- Indices de la tabla `Representantes`
+--
+ALTER TABLE `Representantes`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `cedula` (`cedula`);
 
 --
 -- Indices de la tabla `Usuario`
@@ -140,9 +209,33 @@ ALTER TABLE `Aulas`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT de la tabla `Añoescolar`
+--
+ALTER TABLE `Añoescolar`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `Docentes`
+--
+ALTER TABLE `Docentes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `Estudiantes`
 --
 ALTER TABLE `Estudiantes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `Matricula`
+--
+ALTER TABLE `Matricula`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `Representantes`
+--
+ALTER TABLE `Representantes`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
@@ -154,6 +247,18 @@ ALTER TABLE `Usuario`
 --
 -- Restricciones para tablas volcadas
 --
+
+--
+-- Filtros para la tabla `Aulas`
+--
+ALTER TABLE `Aulas`
+  ADD CONSTRAINT `Aulas_ibfk_1` FOREIGN KEY (`docente_guia`) REFERENCES `Docentes` (`cedula`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `Estudiantes`
+--
+ALTER TABLE `Estudiantes`
+  ADD CONSTRAINT `Estudiantes_ibfk_1` FOREIGN KEY (`cedula_rep`) REFERENCES `Representantes` (`cedula`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `Matricula`
