4765a62e58fa56d88a56b68b0571f91657656953
Andres <andresangel5c@gmail.com>
2024-10-04 12:02:59 -0400

    combios en el codigo fuente

--- Sources/src/sistemamaferu/ConexionBD.java
+++ Sources/src/sistemamaferu/ConexionBD.java
@@ -1,13 +1,34 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package sistemamaferu;
-
-/**
- *
- * @author andres
- */
+import java.sql.*;
+import javax.swing.JOptionPane;
 public class ConexionBD {
     
+   public String db = "sistema";
+   public String url = "jdbc:mysql://localhost/"+db;
+   public String user = "andres";
+   public String pass = "andres";
+   public Statement comando=null;
+   
+   public ConexionBD()
+   {
+     
+   }
+
+   
+   public Connection Conectar()
+   {
+       Connection link = null;
+       try
+       {
+          //Cargamols el Driver MySQL
+           Class.forName ("org.gjt.mm.mysql.Driver");
+            // Creamos un enlace hacia la base de datos
+           link = DriverManager.getConnection(this.url, this.user, this.pass);
+       }
+       catch (Exception e)
+       {
+        JOptionPane.showMessageDialog (null, e);
+       } 
+       return link;
+   }
 }
